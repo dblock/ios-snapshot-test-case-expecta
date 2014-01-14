3eeae7745c55842bf16dffc6e85220cbe99a1ca7
dblock <dblock@dblock.org>
2014-01-14 14:45:11 -0500

    Added demo, copy from https://github.com/facebook/ios-snapshot-test-case, but with S[ecta/Expecta.

--- EXPMatchers+FBSnapshotTest.m
+++ EXPMatchers+FBSnapshotTest.m
@@ -1,1 +1,61 @@
-EXPMatchers+FBSnapshotTest.m
+//
+//  EXPMatchers+FBSnapshotTest.h
+//  Artsy
+//
+//  Created by Daniel Doubrovkine on 1/14/14.
+//  Copyright (c) 2014 Artsy Inc. All rights reserved.
+//
+
+#import "EXPMatchers+FBSnapshotTest.h"
+#import "EXPMatcherHelpers.h"
+#import "FBTestSnapshotController.h"
+#import "FBSnapshotTestRecorder.h"
+
+@implementation EXPExpect (FBSnapshotTest)
++(BOOL)compareSnapshotOfViewOrLayer:(id)viewOrLayer
+                           snapshot:(NSString *)snapshot
+                           testCase:(id)testCase
+                             record:(BOOL)record
+{
+    FBTestSnapshotController * snapshotController = [[FBTestSnapshotController alloc] initWithTestClass:[testCase class]];
+    FBSnapshotTestRecorder * recorder = [[FBSnapshotTestRecorder alloc] initWithController:snapshotController];
+    recorder.selector = NSSelectorFromString(snapshot);
+    recorder.recordMode = record;
+    NSString *referenceImagesDirectory = [NSString stringWithFormat:@"%s", FB_REFERENCE_IMAGE_DIR];
+    __block NSError *error = nil;
+    return [recorder compareSnapshotOfViewOrLayer:viewOrLayer
+                         referenceImagesDirectory:referenceImagesDirectory
+                                       identifier:nil
+                                            error:& error];
+}
+@end
+
+EXPMatcherImplementationBegin(haveValidSnapshot, (NSString * snapshot)) {
+    match(^BOOL{
+        return [EXPExpect compareSnapshotOfViewOrLayer:actual snapshot:snapshot testCase:[self testCase] record:NO];
+    });
+
+    failureMessageForTo(^NSString *{
+        return [NSString stringWithFormat:@"expected a matching snapshot in %@", snapshot];
+    });
+
+    failureMessageForNotTo(^NSString *{
+        return [NSString stringWithFormat:@"expected: not to have a matching snapshot in %@", snapshot];
+    });
+}
+EXPMatcherImplementationEnd
+
+EXPMatcherImplementationBegin(recordSnapshot, (NSString * snapshot)) {
+    match(^BOOL{
+        return [EXPExpect compareSnapshotOfViewOrLayer:actual snapshot:snapshot testCase:[self testCase] record:YES];
+    });
+
+    failureMessageForTo(^NSString *{
+        return [NSString stringWithFormat:@"expected to record a snapshot in %@", snapshot];
+    });
+
+    failureMessageForNotTo(^NSString *{
+        return [NSString stringWithFormat:@"expected: to record a matching snapshot in %@", snapshot];
+    });
+}
+EXPMatcherImplementationEnd
---
+++ FBSnapshotTestCaseDemo/FBSnapshotTestCaseDemoTests/FBSnapshotTestCaseDemoSpecs.m
@@ -0,0 +1,29 @@
+//
+//  FBSnapshotTestCaseDemoSpecs.m
+//  FBSnapshotTestCaseDemo
+//
+//  Created by Daniel Doubrovkine on 1/14/14.
+//  Copyright (c) 2014 Artsy Inc. All rights reserved.
+//
+
+#define EXP_SHORTHAND
+#include <Specta/Specta.h>
+#include <Expecta/Expecta.h>
+#include "EXPMatchers+FBSnapshotTest.h"
+#include "FBExampleView.h"
+
+SpecBegin(FBExampleView)
+
+it(@"matches view", ^{
+    FBExampleView *view = [[FBExampleView alloc] initWithFrame:CGRectMake(0, 0, 64, 64)];
+    expect(view).to.recordSnapshot(@"FBExampleView");
+    expect(view).to.haveValidSnapshot(@"FBExampleView");
+});
+
+it(@"doesn't match a view", ^{
+    FBExampleView *view = [[FBExampleView alloc] initWithFrame:CGRectMake(0, 0, 64, 64)];
+    expect(view).toNot.haveValidSnapshot(@"FBExampleViewDoesNotExist");
+});
+
+SpecEnd
+

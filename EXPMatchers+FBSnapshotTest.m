ed8e84026adf6fc8b67cfa077180b56b518d3beb
Ladislav Martincik <ladislav.martincik@gmail.com>
2014-10-06 10:56:41 +0200

    Changing the code according new impl.
    Ref: https://github.com/specta/specta/issues/105

--- EXPMatchers+FBSnapshotTest.m
+++ EXPMatchers+FBSnapshotTest.m
@@ -112,7 +112,7 @@ void setGlobalReferenceImageDir(char *reference) {
 NSString *sanitizedTestPath();
 
 NSString *sanitizedTestPath(){
-    SPTExample *compiledExample = SPTCurrentSpec;
+    SPTSpec *compiledExample = [[NSThread currentThread] threadDictionary][@"SPTCurrentSpec"];
     NSCharacterSet *charSet = [NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"];
     NSString *currentTestName = [[compiledExample.name componentsSeparatedByCharactersInSet:[charSet invertedSet]] componentsJoinedByString:@"_"];
 

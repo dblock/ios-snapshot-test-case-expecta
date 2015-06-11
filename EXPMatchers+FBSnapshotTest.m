446b7e7b2145b4bb3e0dcacac90d09d5a56f51f2
Ruslan Skorb <ruslan.skorb@gmail.com>
2015-06-12 00:17:56 +0300

    Switch to #if __has_include.
    Fixes #17.

--- EXPMatchers+FBSnapshotTest.m
+++ EXPMatchers+FBSnapshotTest.m
@@ -104,7 +104,7 @@ void setGlobalReferenceImageDir(char *reference) {
 // If you're bringing in Speca via CocoaPods
 // use the test path to get the test's image file URL
 
-#ifdef COCOAPODS_POD_AVAILABLE_Specta
+#if __has_include(<Specta/Specta.h>)
 #import <Specta/Specta.h>
 #import <Specta/SpectaUtility.h>
 #import <Specta/SPTExample.h>

ef41fed4f703dfe1cca1ac440212739b5501e160
brainbicycle <brian.beckerle@artsymail.com>
2024-04-06 13:47:21 -0400

    delete crashing test

--- FBSnapshotTestCaseDemo/FBSnapshotTestCaseDemoTests/FBSnapshotTestCaseDemoSpecs.m
+++ FBSnapshotTestCaseDemo/FBSnapshotTestCaseDemoTests/FBSnapshotTestCaseDemoSpecs.m
@@ -282,40 +282,6 @@ describe(@"snapshots", ^{
             expect(view).toNot.haveValidSnapshotWithTolerance(0.24);
         });
     });
-    
-    it(@"matches view with 256 bytes aligned pointer", ^{
-        Class viewClass = [UIView class];
-        size_t viewInstanceSize = class_getInstanceSize(viewClass);
-        void *ptr = calloc(255 + viewInstanceSize, 1);
-        void *alignedPtr = nil;
-        
-        for (int i = 0; i < 256; ++i) {
-            signed char ptrVal = (signed char)(ptr + i);
-            
-            if (ptrVal == 0) {
-                alignedPtr = ptr + i;
-            }
-        }
-        
-        UIView *view;
-        
-        @autoreleasepool {
-            if (alignedPtr) {
-                id result = (__bridge_transfer id)alignedPtr;
-                object_setClass(result, viewClass);
-                
-                view = [result initWithFrame:frame];
-                CFRetain((__bridge CFTypeRef)view); // prevent ARC from deallocation
-            }
-            
-            expect(view).toNot.recordSnapshot();
-            expect(view).to.haveValidSnapshot();
-            
-//            [view dealloc]; // should be called manually
-        }
-        
-        free(ptr);
-    });
 });
 
 SpecEnd
